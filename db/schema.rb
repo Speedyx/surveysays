75e068837293782a0e6a54aa030ae7a1b2b80e8d
Speedyx <cacope76@hotmail.com>
2014-03-24 00:28:42 +0100

    Añadimos answers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323211150) do
+ActiveRecord::Schema.define(version: 20140323230019) do
+
+  create_table "answers", force: true do |t|
+    t.integer  "question_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.integer  "survey_id"
